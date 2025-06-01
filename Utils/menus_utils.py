b247ff87d34c535b53596a5b5dd7a735edfdd911
tacklezinho <83431002+tacklezinho@users.noreply.github.com>
2025-06-01 14:22:55 -0300

    Projeto Biblioteca
    Adicionar Livros Funcionando
    Retirar Livros Funcionando

---
+++ Utils/menus_utils.py
@@ -0,0 +1,9 @@
+
+
+def menuInicial():
+    print("--------------------")
+    print("1- Adicionar Livro")
+    print("2- Retirar Livro")
+    print("3- Buscar Livro")
+    print("4- Listar Livro")
+    print("--------------------")
---
+++ main.py
@@ -0,0 +1,26 @@
+from Classes.Livro import Livro
+from Classes.Biblioteca import Biblioteca
+from Utils.funcoes_tools import *
+import Utils.menus_utils as menu
+import Utils.fluxos_menus as fluxo
+
+
+
+#Cria a instancia da Biblioteca
+bibliotecaObj = Biblioteca(nomeBiblioteca=input("Digite o nome da sua biblioteca: "))
+
+#Adiciona um livro inicial para Debug
+bibliotecaObj.adicionarLivro(Livro(titulo="Livro Teste", autor="Autor Teste"))
+
+while True:
+    menu.menuInicial()
+    escolhaUser = str(input("\n"))
+    match escolhaUser:
+        case "1":
+            fluxo.fluxoAdicionarLivro(bibliotecaObj, menu)
+        case "2":
+            fluxo.fluxoRetirarLivro(bibliotecaObj, menu)
+        case "3":
+            fluxo.fluxoBuscarLivro(bibliotecaObj, menu)
+        case "4":
+            fluxo.fluxoListarLivros(bibliotecaObj, menu)
